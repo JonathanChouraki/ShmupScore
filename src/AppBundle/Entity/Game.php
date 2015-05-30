c36c664c0619b35b10437c6d9d18c6b2ab2a664d
Jonathan Chouraki <jonathan.chouraki@gmail.com>
2015-05-30 15:40:36 -0400

    Created Game entity

---
+++ src/AppBundle/Entity/Game.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Game
+ *
+ * @ORM\Table(name="ss_game")
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\GameRepository")
+ */
+class Game
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\NotBlank(message="Game name cannot be left blank")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="version", type="string", length=255, nullable=true)
+     */
+    private $version;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="developer", type="string", length=255)
+     * @Assert\NotBlank(message="Game developer cannot be left blank")
+     */
+    private $developer;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Game
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set version
+     *
+     * @param string $version
+     * @return Game
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+
+        return $this;
+    }
+
+    /**
+     * Get version
+     *
+     * @return string 
+     */
+    public function getVersion()
+    {
+        return $this->version;
+    }
+
+    /**
+     * Set developer
+     *
+     * @param string $developer
+     * @return Game
+     */
+    public function setDeveloper($developer)
+    {
+        $this->developer = $developer;
+
+        return $this;
+    }
+
+    /**
+     * Get developer
+     *
+     * @return string 
+     */
+    public function getDeveloper()
+    {
+        return $this->developer;
+    }
+}
